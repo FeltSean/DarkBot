c1972c92b33ce25a308c11827d4307c3acea659f
FeltSean <37269943+FeltSean@users.noreply.github.com>
2019-08-18 17:18:19 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = ("&")
 
-client.login(process.env.TOTKEN);
+client.login(process.env.TOKEN);
 
 process.setMaxListeners(Infinity);
 
