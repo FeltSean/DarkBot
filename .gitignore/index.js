fcb489377bc979afac691de2b5e710744c70526c
FeltSean <37269943+FeltSean@users.noreply.github.com>
2019-08-20 16:17:24 +0200

    Add files via upload

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -24,7 +24,7 @@ client.on('ready', async () =>
 {
     console.log(` ${client.user.username} est en ligne !!! `);
     client.user.setActivity( `,help | Membres : ${client.users.size}` );
-    client.guilds.get("613340625584259091").channels.get("613343531297275904").send(` **```diff -Redémarrage réussi avec succès```** !!! `)
+    client.guilds.get("613340625584259091").channels.get("613343531297275904").send("**```diff -Redémarrage réussi avec succès```** !!!") 
     client.guilds.get("613340625584259091").channels.get("613343531297275904").send(' Prêt à vous servir avec toutes mes fonctionnalités. Commence par faire ,help !!! :desktop: ')
 });
 
